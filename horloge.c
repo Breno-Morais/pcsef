838b543868a908a9506b4eaa6d94c08ce328f829
Breno-Morais <bneromorais@gmail.com>
2023-12-10 21:09:28 +0100

    Horloge back in game

--- horloge.c
+++ horloge.c
@@ -1,5 +1,7 @@
 #include <horloge.h>
 
+int TICKS = 0;
+
 void ecrit_temps(char* s, uint32_t size)
 {
     uint16_t prev_lig, prev_col, col, lig;
@@ -25,5 +27,32 @@ void init_traitant_IT(uint32_t num_IT, void (*traitant)(void))
 }
 
 void tic_PIT(void){
-    traite_car('H');
+    // 0 - acknowledgment of the interrupt
+    outb(0x20,0x20);
+    TICKS+=1;
+    // 1 - create time String
+    char temps[9];
+    sprintf(temps, "%02u:%02u:%02u", (TICKS/(CLOCK_FREQ6060))%24, (TICKS/(CLOCK_FREQ60))%60, (TICKS/CLOCK_FREQ)%60);
+    // 2 - Display
+    ecrit_temps(temps, strlen(temps));
+}
+
+void regler_frequence(void){
+    // 1) Send command 0x34 to port 0x43
+    outb((uint8_t) 0x34, (uint16_t) 0x43);
+    // 2) Send 8 least significant bits
+    outb((QUARTZ / CLOCK_FREQ) & 0xFF, (uint16_t) 0x40);
+    // 3) Send 8 most significant bits
+    outb(((QUARTZ / CLOCK_FREQ) >> 8) & 255, (uint16_t) 0x40);
+}
+
+void masque_IRQ(uint32_t num_IRQ, uint8_t masque){
+    // Read the current mask value
+    uint8_t current_mask = inb(0x21);
+    // Set bit N to zero:
+    current_mask = current_mask & (~(1<<num_IRQ));
+    // Set bit N to masque value
+    current_mask = current_mask | (masque<<num_IRQ);
+    // Send updated mask to port 0x21
+    outb(current_mask, (uint16_t) 0x21);
 }
--- horloge.h
+++ horloge.h
@@ -5,7 +5,11 @@
 #include <ecran.h>
 #include <segment.h>
 
-/* Write the string in the top-right corner */
+#define QUARTZ 0x1234DD
+#define CLOCK_FREQ6060 CLOCK_FREQ60 * 60
+#define CLOCK_FREQ60 CLOCK_FREQ * 60
+#define CLOCK_FREQ 50
+
 void ecrit_temps(char* s, uint32_t size);
 
 void init_traitant_IT(uint32_t num_IT, void (*traitant)(void));
@@ -13,5 +17,9 @@ void init_traitant_IT(uint32_t num_IT, void (*traitant)(void));
 extern void traitant_IT_32(void);
 
 void tic_PIT(void);
+
+void regler_frequence(void);
+
+void masque_IRQ(uint32_t num_IRQ, uint8_t masque);
 
 #endif
